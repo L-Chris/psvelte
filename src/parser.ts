a4d562b27b0eb43f54f61b2fb1b79bbc772f5510
unknown <563303226@qq.com>
2021-02-16 23:45:05 +0800

    feat: parser

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,7 @@
 interface Node {
 	start: number;
 	end: number;
+	name: string;
 	type: string;
 	children?: Node[];
 }
@@ -14,23 +15,49 @@ class Parser {
 	constructor(template: string) {
 		this.index = 0
 		this.template = template.trim()
+		this.stack = []
 
 		this.root = {
 			start: null,
 			end: null,
+			name: 'root',
 			type: 'Fragment',
 			children: []
 		}
 
-		while(this.index < this.template.length) {
-			if (!this.read('<')) {
-				throw new Error('error')
-			} else {
-				const name = this.read_until('>')
+		this.stack.push(this.root)
 
-				console.log('name', name)
+		while(this.index < this.template.length) {
+			if (!this.read('<')) return
+
+			const parent = this.current()
+
+			const is_closing_tag = this.read('/')
+
+			const name = this.read_until(/>/)
+
+			const element = {
+				start: this.index,
+				end: null,
+				type: 'Element',
+				name,
+				children: []
+			}
+
+			this.read('>')
+
+			if (is_closing_tag && parent.name === name) {
+				element.end = this.index
+				this.stack.pop()
+			} else {
+				parent.children.push(element)
+				this.stack.push(element)
 			}
 		}
+	}
+
+	current() {
+		return this.stack[this.stack.length - 1]
 	}
 
 	read(pattern: string) {
@@ -42,22 +69,25 @@ class Parser {
 		return false
 	}
 
-	read_until(pattern: string) {
+	read_until(pattern: RegExp) {
 		const match = this.template.slice(this.index).match(pattern)
 
+		const start = this.index
+
 		if (!match) {
-			this.template.slice(this.index)
+			this.index = this.template.length
+			return this.template.slice(this.index)
 		}
 
-		const start = this.index
 		this.index = start + match.index
-
 		return this.template.slice(start, this.index)
 	}
 }
 
 function parse(str: string) {
-	new Parser(str)
+	const parser = new Parser(str)
+
+	return parser.root
 }
 
 export {
--- types/parser.d.ts
+++ types/parser.d.ts
@@ -1,2 +1,9 @@
-declare function parse(str: string): void;
+interface Node {
+    start: number;
+    end: number;
+    name: string;
+    type: string;
+    children?: Node[];
+}
+declare function parse(str: string): Node;
 export { parse };
